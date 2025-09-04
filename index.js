49b556228938d5284138d19cd691b402aec70aed
Cain Tapp <cainlucastapp@gmail.com>
2025-09-04 14:56:06 -0700

    Login Count Logic

--- index.js
+++ index.js
@@ -1,4 +1,25 @@
+function createLoginTracker(userInfo) {
+  let attemptCount = 0;
 
+  const login = (passwordAttempt) => {
+    if (attemptCount >= 3) {
+      return "Account locked due to too many failed login attempts";
+    }
+    if (passwordAttempt === userInfo.password) {
+      return "Login successful";
+    } else {
+      attemptCount++;
+      return `Attempt ${attemptCount}: Login failed`;
+    }
+  };
+  return login;
+}
+
+
+const userInfoExample = {
+  "username": "user1",
+  "password": "password123"
+};
 
 
 module.exports = {
